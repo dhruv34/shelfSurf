9146e5b53093a2014b11d3872d9b959a0cf7ed4c
Dhruv R. Adha <dhruv@Dhruvs-MacBook-Air-2.local>
2024-12-18 21:43:39 -0500

    Basic template set up for React Native, Expo, and NativeWind

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl">Hello World! Introducing Shelf Surf!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
+    </View>
+  );
+}
